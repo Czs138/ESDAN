2bd0d0fdedb37907e951096823795eaed6d333a7
Czs138 <czs@henu.edu.cn>
2024-07-30 22:46:41 +0800

    Update run_scripts.sh

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,13 +1,13 @@
 # Testing x2, x3, x4
-python test.py -opt options/test/test_PANx2.yml
-python test.py -opt options/test/test_PANx3.yml
-python test.py -opt options/test/test_PANx4.yml
+python test.py -opt options/test/test_ESDANx2.yml
+python test.py -opt options/test/test_ESDANx3.yml
+python test.py -opt options/test/test_ESDANx4.yml
 
 
 # Training x2, x3, x4
-python train.py -opt options/train/train_PANx2.yml
-python train.py -opt options/train/train_PANx3.yml
-python train.py -opt options/train/train_PANx4.yml
+python train.py -opt options/train/train_ESDANx2.yml
+python train.py -opt options/train/train_ESDANx3.yml
+python train.py -opt options/train/train_ESDANx4.yml
 
 
 # Training SRResNet_PA or RCAN_PA
